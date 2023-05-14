f12cdf9528f2bef900358e46586eb435ee64ba91
houcha <houcha@github.com>
2023-05-14 19:18:57 +0000

    Add dictionaries download scripts

---
+++ baseline/dicts/download_dictionaries.sh
@@ -0,0 +1,10 @@
+# Zaliznyak
+./download_from_yandex_disk.py https://disk.yandex.ru/d/Q8tM7IwEQafyY .
+unzip dict_zaliz_utf8.zip
+cat dict/* > Zaliz.txt
+rm -rf dict dict_zaliz_utf8.zip
+# Compreno full
+./download_from_yandex_disk.py https://disk.yandex.ru/d/hlQ77INmJzxVdg .
+unrar x MorphoDic_full_utf8.rar
+mv MorphoDic_OUT.txt ComprenoFull.txt
+rm "MorphoDic_OUT - копия.txt" MorphoDic_full_utf8.rar MorphoDic_full_utf8.txt
