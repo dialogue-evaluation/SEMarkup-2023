792c36da6a68aabf916e37d4c399e0cb0a62df3b
houcha <houcha@github.com>
2023-06-02 17:41:51 +0000

    Change download_dictionaries.sh

--- parsers/dicts/download_dictionaries.sh
+++ parsers/dicts/download_dictionaries.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+# Compreno
+./download_from_yandex_disk.py https://disk.yandex.ru/d/hlQ77INmJzxVdg .
+unrar x MorphoDic_full_utf8.rar MorphoDic_OUT.txt
+mv MorphoDic_OUT.txt ComprenoFull.txt
+rm MorphoDic_full_utf8.rar
+
 # Zaliznyak
 ./download_from_yandex_disk.py https://disk.yandex.ru/d/Q8tM7IwEQafyY .
 unzip dict_zaliz_utf8.zip
 cat dict/* > Zaliz.txt
-rm -rf dict dict_zaliz_utf8.zip
+rm -rf dict/ dict_zaliz_utf8.zip
 
-# Compreno
-./download_from_yandex_disk.py https://disk.yandex.ru/d/hlQ77INmJzxVdg .
-unrar x MorphoDic_full_utf8.rar
-mv MorphoDic_OUT.txt ComprenoFull.txt
-rm "MorphoDic_OUT - копия.txt" MorphoDic_full_utf8.rar MorphoDic_full_utf8.txt
